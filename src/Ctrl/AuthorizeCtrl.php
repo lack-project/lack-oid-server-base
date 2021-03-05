59ebd8a10719ee9f99e081af1da8bfc185c40faa
Matthes <matthes@leuffen.de>
2021-03-05 15:12:56 +0100

    updae

--- src/Ctrl/AuthorizeCtrl.php
+++ src/Ctrl/AuthorizeCtrl.php
@@ -4,11 +4,13 @@
 namespace Lack\OidServer\Base\Ctrl;
 
 
+use Lack\OidServer\Base\Type\T_Q_Authorize;
+
 class AuthorizeCtrl
 {
 
 
-    public function __invoke()
+    public function __invoke(T_Q_Authorize $query)
     {
 
     }
